0754ac1b218c354036c916b883b18965c1c5beab
david <mail@nonswag.dev>
2026-06-24 16:51:08 +0200

    Fix nukkit example plugin

--- nukkit/example-plugin/src/main/java/com/example/ExamplePlugin.java
+++ nukkit/example-plugin/src/main/java/com/example/ExamplePlugin.java
@@ -11,20 +11,25 @@ public final class ExamplePlugin extends PluginBase {
     public static final ErrorTracker ERROR_TRACKER = ErrorTracker.contextAware();
     private final AtomicInteger gameCount = new AtomicInteger();
 
-    private final NukkitContext context = new NukkitContext.Factory(this, "YOUR_TOKEN_HERE")
-            .errorTrackerService(ERROR_TRACKER)
-            // .metrics(Metrics.Factory::create) // Define a minimal metrics instance without any custom metrics
-            .metrics(factory -> factory
-                    // Custom metrics require a corresponding data source in your project settings
-                    .addMetric(Metric.number("game_count", gameCount::get))
-                    .addMetric(Metric.string("server_version", () -> "1.0.0"))
+    private NukkitContext context;
 
-                    // #onFlush is invoked after successful metrics submission
-                    // This is useful for cleaning up cached data
-                    .onFlush(() -> gameCount.set(0)) // reset game count on flush
+    @Override
+    public void onLoad() {
+        context = new NukkitContext.Factory(this, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                .errorTrackerService(ERROR_TRACKER)
+                // .metrics(Metrics.Factory::create) // Define a minimal metrics instance without any custom metrics
+                .metrics(factory -> factory
+                        // Custom metrics require a corresponding data source in your project settings
+                        .addMetric(Metric.number("game_count", gameCount::get))
+                        .addMetric(Metric.string("server_version", () -> "1.0.0"))
 
-                    .create())
-            .create();
+                        // #onFlush is invoked after successful metrics submission
+                        // This is useful for cleaning up cached data
+                        .onFlush(() -> gameCount.set(0)) // reset game count on flush
+
+                        .create())
+                .create();
+    }
 
     @Override
     public void onEnable() {
